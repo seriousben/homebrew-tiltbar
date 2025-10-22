c486f6264bcc7f9c7a0580891f7d2ef7467d0657
Benjamin Boudreau <boudreau.benjamin@gmail.com>
2025-10-22 14:23:08 -0400

    Stop requiring xcode being installed

--- Formula/tiltbar.rb
+++ Formula/tiltbar.rb
@@ -16,7 +16,6 @@ class Tiltbar < Formula
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "Apache-2.0"
 
-  depends_on xcode: ["14.0", :build]
   depends_on :macos => :ventura
 
   def install
